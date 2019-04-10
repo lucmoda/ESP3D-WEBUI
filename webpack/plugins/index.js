339a805231d34575f616c0b2c6e80d6ce1b6e50c
Luc <luc@tech-hunters.com>
2019-04-10 21:38:34 +0200

    Replace clean webpack plugin by remove files webpack plugin for production
    Add html minify for production
    Add css js merge to html for production
    Add compression to index.html.gz for production

--- webpack/plugins/index.js
+++ webpack/plugins/index.js
@@ -1,7 +1,16 @@
+import compressionPlugin from "./compression-plugin"
+import htmlWebpackInlineSourcePlugin from "./html-webpack-inline-source-plugin"
 import htmlWebpackPlugin from "./html-webpack-plugin"
-import cleanWebpackPlugin from "./clean-webpack-plugin"
+import htmlMinifierWebpackPlugin from "./html-minifier-webpack-plugin"
+import removeFilesWebpackPlugin from "./remove-files-webpack-plugin"
 
 export const environmentPlugins = [
-  htmlWebpackPlugin(),
-  cleanWebpackPlugin(),    
+  htmlWebpackPlugin(),   
+]
+
+export const productionPlugins = [
+  htmlWebpackInlineSourcePlugin(),
+  htmlMinifierWebpackPlugin(),
+  compressionPlugin(),
+  removeFilesWebpackPlugin()
 ]
--- webpack/production.js
+++ webpack/production.js
@@ -1,6 +1,7 @@
 import merge from "webpack-merge"
 import environment from "./environment"
+import { productionPlugins } from "./plugins"
 
 module.exports = merge(environment, {
-
+	plugins: productionPlugins
 })
